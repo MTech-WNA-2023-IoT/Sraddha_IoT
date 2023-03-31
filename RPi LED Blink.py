8d3e5ff0d25d3fb63a051f09d4496e37589dc81e
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-03-31 10:39:30 +0530

    Update and rename RPi LED Blink to RPi LED Blink.py

---
+++ RPi LED Blink.py
@@ -0,0 +1,14 @@
+#Import GPIO Library
+import RPi.GPIO as GPIO
+#Import time function Library
+import time
+#Configure GPIO in Raspberry Pi BCM Mode
+GPIO.setmode(GPIO.BCM) 
+#Configure GPIO Pin 17 as output
+GPIO.setup(17,GPIO.OUT) 
+#Set GPIO Pin to High
+GPIO.output(17,GPIO.HIGH)
+#Wait for 1sec
+time.sleep(1)
+#Set GPIO Pin to Low
+GPIO.output(17,GPIO.LOW)
