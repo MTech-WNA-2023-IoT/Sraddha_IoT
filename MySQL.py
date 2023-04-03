511869f92626c951d464bbcdc777bcfc1375e371
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-04-03 10:22:29 +0530

    Update MySQL.py

--- MySQL.py
+++ MySQL.py
@@ -1,1 +1,17 @@
+#Import pymysql module library
+import pymysql
+#Create a connection to MySQL Database 
+conn =pymysql.connect(database="Sraddha_1",user="sraddha",password="sraddha@99",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, topic, data
+data={'Topic':'Humidity Sensor','Data':12.5}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO MQTT_1(Topic, Data)VALUES(%(Topic)s,%(Data)s);",data)
+#Close the cursor
+cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
 
