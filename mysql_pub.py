6d3a0219e9c62a9062d688779af10c5559c25596
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-04-10 10:22:35 +0530

    Create mysql_pub.py

---
+++ mysql_pub.py
@@ -0,0 +1,9 @@
+import paho.mqtt.client as mqtt
+#pip3 install paho-mqtt
+
+payload="Hello:
+topic="IOT/test"
+client = mqtt.Client()
+client.connect('http://34.100.205.14/',1883,60)
+(rc,mid)=client.publish(topic,payload);
+client.disconnect();
