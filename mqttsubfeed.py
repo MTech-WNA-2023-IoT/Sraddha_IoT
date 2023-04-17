2fa8d320688611039bf3e670a78a046feca00bea
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-04-17 13:04:20 +0530

    Update mqttsubfeed.py

--- mqttsubfeed.py
+++ mqttsubfeed.py
@@ -35,4 +35,19 @@ mqttclient.on_connect = _on_connect
 
 mqttclient.loop_forever()
 
-
+#Import pymysql module library
+import pymysql
+#Create a connection to MySQL Database 
+conn =pymysql.connect(database="Sraddha_1",user="sraddha",password="sraddha@99",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, topic,data
+data={'Topic:ph level','Data: 7.41'}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO MQTT_1(Topic,Data)VALUES(%(topic)s,%(data)s;",data)
+#Close the cursor
+cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
