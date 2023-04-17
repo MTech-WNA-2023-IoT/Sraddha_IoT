39d582ba2f28fb35197b25861d0a482560531dd5
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-04-17 12:16:32 +0530

    Update mysqlfeed.py

--- mysqlfeed.py
+++ mysqlfeed.py
@@ -1,13 +1,13 @@
 #Import pymysql module library
 import pymysql
 #Create a connection to MySQL Database 
-conn =pymysql.connect(database="MQTT_1",user="sraddha",password="sraddha@99",host="localhost")
+conn =pymysql.connect(database="Sraddha_1",user="sraddha",password="sraddha@99",host="localhost")
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, topic,data
 data={'Topic:ph level','Data: 7.41'}
 #Execute the SQL to write data to the database
-cur.execute("INSERT INTO <Sraddha_1>(Topic,Data)VALUES(%(topic)s,%(data)s;",data)
+cur.execute("INSERT INTO MQTT_1(Topic,Data)VALUES(%(topic)s,%(data)s;",data)
 #Close the cursor
 cur.close()
 #Commit the data to the database
